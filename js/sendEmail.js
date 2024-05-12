b3d9bbdc80ccd46298225853b9720c958b2308e8
puspakmohanty <84153185+sonic-afkk@users.noreply.github.com>
2024-05-12 19:14:55 +0530

    Update sendEmail.js

--- js/sendEmail.js
+++ js/sendEmail.js
@@ -4,9 +4,8 @@ const submitBtn = document.querySelector("#submit-btn");
 const popup = document.querySelector(".popup");
 const popupCloseBtn = document.querySelector(".popup-close");
 
-// API URL and API KEY
 const APIURL = "https://api.web3forms.com/submit";
-const APIKEY = myEnv.getVariable("web3FormsApiKey");
+const APIKEY = "65df8834-b54f-4683-b208-9bfb5a1c751f";
 
 // form submit event
 form.addEventListener("submit", async (e) => {
